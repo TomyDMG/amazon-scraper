7b01b9bed805668a34028fa632132fe9d07072f3
M <tomydmg@gmail.com>
2016-07-17 15:32:57 +0300

    generateUrl() -> generateASIN(), checkUrl() -> checkASIN, add googleCheck()

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ import re
 import ConfigParser
 import random
 import string
+import anydbm
 from lxml import html
 from google import search
 
+#db = anydbm.open('cache', 'n')
 config = ConfigParser.RawConfigParser()
 config.readfp(open('defaults.cfg'))
 google_scan_urls = config.getboolean('section', 'google_scan_urls')
 threads_count = config.getint('section', 'threads_count')
 
 
-def generateUrl():
+def generateASIN():
     asin = 'B00' + \
         ''.join(random.choice(string.ascii_uppercase + string.digits)
                 for _ in range(7))
-    return 'https://www.amazon.com/dp' + asin
+    return asin
 
 
-def checkUrl(url):
+def checkASIN(asin):
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+    url = 'https://www.amazon.com/dp/' + asin
     page = requests.get(url, headers=headers)
     html_tree = html.fromstring(page.content)
     if html_tree.xpath('//img[@src="https://images-na.ssl-images-amazon.com/images/G/01/error/title._TTD_.png"]') or html_tree.xpath('//div[@id="outOfStock"]'):
@@ -29,13 +32,18 @@ def checkUrl(url):
     else:
         return False
 
-generateUrl()
-checkUrl(generateUrl())
+
+def googleCheck():
+    count = 0
+    for url in search(generateASIN(), stop=config.getint('section', 'google_min_results')):
+        print (url)
+        count += 1
+        print count
+
+
+asin = generateASIN()
+checkUrl(asin)
+asin
 checkUrl('https://www.amazon.com/dp/B00ATDPHTC')
 checkUrl('https://www.amazon.com/dp/B00QKGAOSQ')
 checkUrl('https://www.amazon.com/dp/B00GOVNISHE')
-
-
-def googleCheck():
-    for url in search(asin, stop=config.getint('section', 'google_min_results')):
-        print(url)
