9f87608e94af4e28357af4e09aca07fb9aa83a74
M <tomydmg@gmail.com>
2016-07-17 17:50:07 +0300

    Add isASINAlreadyChecked(), writeASINToDB(), googleCheck(). Modified checkASIN()

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import re
 import ConfigParser
 import random
 import string
-import anydbm
+import sqlite3
 from lxml import html
 from google import search
 
-#db = anydbm.open('cache', 'n')
+db = sqlite3.connect('cache.db')
+
+
 config = ConfigParser.RawConfigParser()
 config.readfp(open('defaults.cfg'))
 google_scan_urls = config.getboolean('section', 'google_scan_urls')
@@ -27,22 +29,45 @@ def checkASIN(asin):
     url = 'https://www.amazon.com/dp/' + asin
     page = requests.get(url, headers=headers)
     html_tree = html.fromstring(page.content)
-    if html_tree.xpath('//img[@src="https://images-na.ssl-images-amazon.com/images/G/01/error/title._TTD_.png"]') or html_tree.xpath('//div[@id="outOfStock"]'):
+    if html_tree.xpath('//img[@src="https://images-na.ssl-images-amazon.com/images/G/01/error/title._TTD_.png"]'):
+        status = 'BLOCKED'
+        return status
+    elif html_tree.xpath('//div[@id="outOfStock"]'):
+        status = 'ABANDONED'
+        return status
+    else:
+        return False
+
+
+def googleCheck(asin):
+    count = 0
+    for url in search(asin, stop=config.getint('section', 'google_min_results')):
+        count += 1
+    if count != 0:
         return True
     else:
         return False
 
 
-def googleCheck():
-    count = 0
-    for url in search(generateASIN(), stop=config.getint('section', 'google_min_results')):
-        print (url)
-        count += 1
-        print count
+def isASINAlreadyChecked(asin):
+    cur = db.cursor()
+    cur.execute("SELECT * FROM amazon_items WHERE asin = '%s'" % asin)
+    if cur.fetchall():
+        return True
+    else:
+        return False
 
+
+def writeASINToDB(asin):
+    cur = db.cursor()
+    url = 'https://www.amazon.com/dp/' + asin
+    status = checkASIN(asin)
+    cur.execute("INSERT INTO amazon_items VALUES ('%s', '%s', '%s')" % (asin, url, status))
+    db.commit()
 
 asin = generateASIN()
-checkUrl(asin)
+asin = 'B00ATDPHTC'
+checkASIN(asin)
 asin
 checkUrl('https://www.amazon.com/dp/B00ATDPHTC')
 checkUrl('https://www.amazon.com/dp/B00QKGAOSQ')
